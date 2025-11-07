354bf4ef477c76741e250aa6c59a281c29179dda
Dan Zarzycki <dzarzycki@me.com>
2025-11-06 22:45:39 -0500

    Start game
    Character on screen can go up and down, and player can start the game

---
+++ Scripts/big_string_player.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+const GRAVITY : int = 1000
+const MAX_VEL : int = 600
+const CLIMB_SPEED : int = -400
+var climbing : bool = false
+var falling : bool = false
+const START_POS = Vector2(100, 400)
+
+# Called when node enters the scene tree for the first time
+func _ready():
+	reset()
+
+
+func reset():
+	falling = false
+	climbing = false
+	position = START_POS
+
+# Called every frame. 'delta' is the elapsed time since the previous frame
+func _physics_process(delta):
+	if climbing or falling:
+		velocity.y += GRAVITY * delta
+		#terminal velocity
+		if velocity.y > MAX_VEL:
+			velocity.y = MAX_VEL
+		move_and_collide(velocity * delta)
+
+func climb():
+	velocity.y = CLIMB_SPEED
